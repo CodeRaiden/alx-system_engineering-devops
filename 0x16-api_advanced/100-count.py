360dbf96e2543131c65d2df27e113a5de385d492
CodeRaiden <hadoken10@yahoo.com>
2023-01-31 11:06:22 -0800

    add recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords (case-insensitive, delimited by spaces. Javascript should count as javascript, but java should not)

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,35 +1,51 @@
 #!/usr/bin/python3
-"""
-Show number of occurrences of keywords in hot post titles (case-insensitive)
-"""
-import re
+"""Recurse function definition"""
 import requests
 
-API = 'https://www.reddit.com/r/{}/hot.json'
+
+def count_words(subreddit, word_list):
+    """count_words function to return printed word counts"""
+    countDict = word_count(subreddit, word_list)
+    for key in [v[0] for v in
+                sorted(countDict.items(), key=lambda kv: (-kv[1], kv[0]))]:
+        if countDict[key] > 0:
+            print("{}: {}".format(key, countDict[key]))
 
 
-def count_words(subreddit, wordlist, nums=None, after=None):
+def word_count(subreddit, word_list, after=None, countDict={}):
     """
-    Query reddit for hot posts and print total occurrences of each keyword
+    Recurse function recursively produces a list of all hot articles
+    on a subreddit
     """
-    r = requests.get(
-        API.format(subreddit),
-        headers={'User-Agent': 'Mozilla/5.0'},
-        params={'after': after, 'limit': 100},
-        allow_redirects=False,
-    )
-    if r.status_code == 200:
-        nums = nums or dict.fromkeys(wordlist, 0)
-        data = r.json()['data']
-        page = [word for post in data['children']
-                for word in post['data']['title'].split()]
-        for key in wordlist:
-            for word in page:
-                if key.casefold() == word.casefold():
-                    nums[key] += 1
-        if data['after'] is None:
-            keys = sorted(filter(nums.get, nums), key=lambda k: (-nums[k], k))
-            for key in keys:
-                print('{}: {}'.format(key, nums[key]))
-        else:
-            count_words(subreddit, wordlist, nums, data['after'])
+    subRcheck = requests.get("https://reddit.com/api/search_reddit_names.json",
+                             headers={
+                                 'User-Agent': 'Safari 12.1'
+                             },
+                             params={
+                                 'exact': True,
+                                 'query': subreddit
+                             })
+    if 'error' in subRcheck.json().keys():
+        pass
+    response = requests.get("https://reddit.com/r/{}.json"
+                            .format(subreddit),
+                            headers={
+                                'User-Agent': 'Safari 12.1'
+                            },
+                            params={
+                                'after': after
+                            })
+    """Create dictionary for count record"""
+    for child in response.json().get('data').get('children'):
+        for word in child.get('data').get('title').split():
+            for wordList in word_list:
+                if wordList not in countDict.keys():
+                    countDict[wordList] = 0
+                if word.upper() == wordList.upper():
+                    countDict[wordList] += 1
+    if response.json().get('data').get('after') is None:
+        return countDict
+    return word_count(subreddit,
+                      word_list,
+                      response.json().get('data').get('after'),
+                      countDict)
