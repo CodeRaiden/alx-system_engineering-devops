5de60147782ed59d52809ddd367fa5c249a79f58
CodeRaiden <hadoken10@yahoo.com>
2023-01-31 11:03:22 -0800

    add recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords (case-insensitive, delimited by spaces. Javascript should count as javascript, but java should not)

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
-"""Script parses the title of all hot articles, and prints a sorted count of given keywords"""
-import requests
+"""
+Show number of occurrences of keywords in hot post titles (case-insensitive)
+"""
 import re
+import requests
 
-URL = 'https://www.reddit.com/r/{}/hot.json'
-USER_AGENT = 'Safari 12.1'
+API = 'https://www.reddit.com/r/{}/hot.json'
 
 
 def count_words(subreddit, wordlist, nums=None, after=None):
-    """Gets reddit for hot posts and print total occurances of each keyword"""
-    resp = requests.get(
-            URL.format(subreddit),
-            headers={'User-Agent': USER_AGENT},
-            params={'after': after, 'limit': 100},
-            allow_redirects=False,
-            )
-    if resp.status_code == 200:
+    """
+    Query reddit for hot posts and print total occurrences of each keyword
+    """
+    r = requests.get(
+        API.format(subreddit),
+        headers={'User-Agent': 'Mozilla/5.0'},
+        params={'after': after, 'limit': 100},
+        allow_redirects=False,
+    )
+    if r.status_code == 200:
         nums = nums or dict.fromkeys(wordlist, 0)
-        data = resp.json()['data']
+        data = r.json()['data']
         page = [word for post in data['children']
                 for word in post['data']['title'].split()]
         for key in wordlist:
