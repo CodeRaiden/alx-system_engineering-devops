248c901d2df79d49bd517fc2207f91a8d0f7f95f
CodeRaiden <hadoken10@yahoo.com>
2023-01-31 07:01:32 -0800

    add function that queries the Reddit API and returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/user/bin/python3
+"""Script returns the number of subscribers"""
+import requests
+
+URL = 'https://www.reddit.com/r/{}/about.json'
+USER_AGENT = 'Safari 12.1'
+def number_of_subscribers(subreddit):
+    """Get number of subreddit subscribers"""
+    resp = requests.get(
+            URL.format(subreddit),
+            headers={'User-Agent': USER_AGENT},
+            allow_redirects=False,
+            timeout=10
+            )
+    if resp.status_code == 200:
+        return resp.json()['data']['subscribers']
+    else:
+        return 0
