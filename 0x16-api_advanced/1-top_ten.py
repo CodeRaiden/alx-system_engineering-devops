4aafc6e6d3f59eff24f4c37fe02b556595c79b2c
CodeRaiden <hadoken10@yahoo.com>
2023-01-31 07:31:19 -0800

    add function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Script queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit"""
+import requests
+
+URL = 'https//www.reddit.com/r/{}/hot.json'
+USER_AGENT = 'Safari 12.1'
+
+
+def top_ten(subreddit):
+    """Gets and prints the titles of the first 10 hot posts"""
+    resp = requests.get(
+            URL.format(subreddit),
+            headers={'User-Agent': USER_AGENT},
+            params={'limit': 10},
+            allow_redirects=False,
+            timeout=10
+            )
+    if resp.status_code == 200:
+        for post in resp.json()['data']['children']:
+            print(post['data']['title'])
+        else:
+            print('None')
